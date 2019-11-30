b874a483b64432ed42f8fb775b36530c6e0dcf23
muhammet ölker <olkerr@gmail.com>
2019-11-30 19:33:49 +0300

    30,11,2019

---
+++ FormApp/FormApp/Controllers/ContactController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Send([FromBody]Formapp.Data.ContactSendto contactSendto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest
+            }
+            return new JsonResult("ok");
+        }
+    }
+}
---
+++ FormApp/Formapp.Data/ContactSendto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Formapp.Data
+{
+    public class ContactSendto
+    { 
+        [Required,MinLength(2),MaxLength(150)]
+        public string FullName { get; set; }
+        [Required, MinLength(10), MaxLength(10)]
+        public string Email { get; set; }
+        [Required, MinLength(10), MaxLength(10)]
+        public string Message { get; set; }
+    
+    }
+}
